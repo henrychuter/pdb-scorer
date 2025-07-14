917ef9a87b63e43d07621c5f8355d23f7d699fd2
Henry Chuter <henrychuter1@gmail.com>
2025-07-13 21:52:34 -0500

    added amino acid sequence

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ init()
 import tkinter as tk
 from tkinter import filedialog, Button, Label
 
+import pymol
 
 def file_upload_score():
     filepath = filedialog.askopenfilename(
@@ -26,6 +27,9 @@ def file_upload_score():
             #in depth breakdown
             pose_energies = pose.energies()
             score_types = scorefxn.get_nonzero_weighted_scoretypes()
+
+            #getting amino acid sequence
+            amino_acids = pose.sequence()
 
 
             score_descriptions = {
@@ -51,22 +55,58 @@ def file_upload_score():
                 'ScoreType.rama_prepro': 'Ramachandran energy preferences with proline'
             }
 
-            protein_score = [f'{round_score} \nIn-depth breakdown:\n']
+            empty_space = []
+
+            reg_header = f'Composite Score: {round_score}\n{"-" * 80}\nIn-Depth Breakdown\n'
+            footer = f'\n{"-" * 80}'
 
             for score_type in score_types:
                 weight = scorefxn.get_weight(score_type)
                 energy = pose_energies.total_energies()[score_type]
                 weighted_energy = weight * energy
 
+
                 display_name = score_descriptions.get(str(score_type), str(score_type))
-                protein_score.append(f'{display_name}: {round(weighted_energy, 2)} (weight: {round(weight, 3)})')
+
+                empty_space.append(f'{display_name:<55}'
+                                   f'{round(weighted_energy, 2):<8} (weight: {round(weight, 3)})\n')
 
 
-            total_breakdown = '\n'.join(protein_score)
-            label.config(text=f'Composite Score(negative is better): {total_breakdown}')
+            total_breakdown = tk.Text(window, font=('Courier New', 14), wrap = 'word', height = 36, width = 75,
+                                      bg=window.cget('bg'), relief='flat', borderwidth=0)
+            total_breakdown.pack(anchor = 'w', padx = 10)
+
+
+            def adding_text():
+                total_breakdown.delete('1.0', tk.END)
+                total_breakdown.insert(tk.END, reg_header + f'\n{"".join(empty_space)}' + footer
+                                       + f'\nAmino Acid Sequence\n\n{amino_acids}\n' + footer)
+                total_breakdown.config(state='disabled')
+
+                reset_button.pack(pady = 10)
+
+                label.pack_forget()
+                upload_button.pack_forget()
+
+            adding_text()
 
         except Exception as e:
             label.config(text=f'Error :( :\n{e}')
+
+def reset_fxn():
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    global label, upload_button, reset_button
+
+    label = Label(window, text='No file selected', wraplength=750)
+    label.pack(pady=10)
+
+    upload_button = Button(window, text='Upload file(.pdb)', command = file_upload_score)
+    upload_button.pack(pady=10)
+
+    reset_button = Button(window, text='Reset Window', command = reset_fxn)
+
 window = tk.Tk()
 window.title('Protein scorer')
 window.geometry('900x600')
@@ -75,9 +115,9 @@ window.geometry('900x600')
 label = Label(window, text = 'No file selected', wraplength = 750)
 label.pack(pady = 10)
 
-
 upload_button = Button(window, text = 'Upload file(.pdb)', command = file_upload_score)
 upload_button.pack(pady = 10)
 
+reset_button = Button(window, text='Reset Window', command = reset_fxn)
 
 window.mainloop()
